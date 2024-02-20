a56dbd73d23723797e62b007604ac22caa930d85
Vitor Carvalho <vitorcarvalho.014@gmail.com>
2024-02-19 21:30:55 -0300

    Update app.tsx

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-const note = {
-  date: new  Date(),
-  content: 'Hello World'
-}
 
-export function App() {  
+export function App() { 
+  
+  const [notes, setNotes] = useState ([
+    { id: 1, date: new  Date(), content: 'Hello World' },
+    { id: 2, date: new  Date(), content: 'Nota 2' }
+  ])
+  
   return (    
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt='NLW Expert' />
@@ -18,11 +21,19 @@ export function App() {
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'  
         />
       </form>
+
       <div className="h-px bg-slate-700" />
+
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
 
         <NewNoteCard />
-        <NoteCard note={note} />
+
+        {notes.map ( note => {
+          return <NoteCard note = { note } />
+        })}
+
+        
+
         
       </div>
     </div>
